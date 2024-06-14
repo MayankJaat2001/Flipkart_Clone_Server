3f9cdb5bb933c97faa513bf8b7752d3cd85e3ad0
Mayank Chaudhary <146983418+MayankJaat2001@users.noreply.github.com>
2024-06-15 01:45:39 +0530

    Update default.js

--- default.js
+++ default.js
@@ -3,7 +3,6 @@ import { products } from './constants/data.js'
 import Product from './model/product-schema.js';
 const DefaultData=async()=>{
     try{
-        await Product.deleteMany({});
         await Product.insertMany(products);
         console.log('Data imported Successfully'); 
     }
